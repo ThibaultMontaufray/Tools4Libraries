cb4f626ba274c408688127f68a2ffd4f7e6a307d
Thibault MONTAUFRAY <amostrack@gmail.com>
2017-03-11 07:29:44 +1300

    update panel custom

---
+++ Graphics/PanelCustom.cs
@@ -0,0 +1,220 @@
+﻿using System.Windows.Forms;
+
+namespace Tools4Libraries
+{
+    public class PanelCustom : UserControl
+    {
+        #region Attribute
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.Panel panelTop;
+        private System.Windows.Forms.Panel panelBottom;
+        private System.Windows.Forms.Panel panelRight;
+        private System.Windows.Forms.Panel panelLeft;
+        private System.Windows.Forms.Panel panelTopMiddle;
+        private System.Windows.Forms.Panel panelTopRight;
+        private System.Windows.Forms.Panel panelTopLeft;
+        private System.Windows.Forms.Panel panelBottomMiddle;
+        private System.Windows.Forms.Panel panelBottomRight;
+        private System.Windows.Forms.Panel panelBottomLeft;
+
+        public System.Windows.Forms.Panel panelMiddle;
+        public UserControlCustom _userControl;
+        #endregion
+
+        #region Properties
+        #endregion
+
+        #region Constructor
+        public PanelCustom()
+        {
+            InitializeComponent();
+        }
+        public PanelCustom(UserControlCustom userControl)
+        {
+            _userControl = userControl;
+            InitializeComponent();
+            LoadUserControl();
+        }
+        #endregion
+
+        #region Methods public
+        public void RefreshData()
+        {
+            if (_userControl != null)
+            {
+                _userControl.RefreshData();
+            }
+        }
+        public void ChangeLanguage()
+        {
+            if (_userControl != null)
+            {
+                _userControl.ChangeLanguage();
+            }
+        }
+        #endregion
+
+        #region Methods protected
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+        #endregion
+
+        #region Methods private
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PanelCustom));
+            this.panelTop = new System.Windows.Forms.Panel();
+            this.panelTopMiddle = new System.Windows.Forms.Panel();
+            this.panelTopRight = new System.Windows.Forms.Panel();
+            this.panelTopLeft = new System.Windows.Forms.Panel();
+            this.panelBottom = new System.Windows.Forms.Panel();
+            this.panelBottomMiddle = new System.Windows.Forms.Panel();
+            this.panelBottomRight = new System.Windows.Forms.Panel();
+            this.panelBottomLeft = new System.Windows.Forms.Panel();
+            this.panelRight = new System.Windows.Forms.Panel();
+            this.panelLeft = new System.Windows.Forms.Panel();
+            this.panelMiddle = new System.Windows.Forms.Panel();
+            this.panelTop.SuspendLayout();
+            this.panelBottom.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panelTop
+            // 
+            this.panelTop.Controls.Add(this.panelTopMiddle);
+            this.panelTop.Controls.Add(this.panelTopRight);
+            this.panelTop.Controls.Add(this.panelTopLeft);
+            this.panelTop.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelTop.Location = new System.Drawing.Point(0, 0);
+            this.panelTop.Name = "panelTop";
+            this.panelTop.Size = new System.Drawing.Size(600, 25);
+            this.panelTop.TabIndex = 0;
+            // 
+            // panelTopMiddle
+            // 
+            this.panelTopMiddle.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panelTopMiddle.BackgroundImage")));
+            this.panelTopMiddle.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelTopMiddle.Location = new System.Drawing.Point(25, 0);
+            this.panelTopMiddle.Name = "panelTopMiddle";
+            this.panelTopMiddle.Size = new System.Drawing.Size(550, 25);
+            this.panelTopMiddle.TabIndex = 2;
+            // 
+            // panelTopRight
+            // 
+            this.panelTopRight.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panelTopRight.BackgroundImage")));
+            this.panelTopRight.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panelTopRight.Location = new System.Drawing.Point(575, 0);
+            this.panelTopRight.Name = "panelTopRight";
+            this.panelTopRight.Size = new System.Drawing.Size(25, 25);
+            this.panelTopRight.TabIndex = 1;
+            // 
+            // panelTopLeft
+            // 
+            this.panelTopLeft.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panelTopLeft.BackgroundImage")));
+            this.panelTopLeft.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panelTopLeft.Location = new System.Drawing.Point(0, 0);
+            this.panelTopLeft.Name = "panelTopLeft";
+            this.panelTopLeft.Size = new System.Drawing.Size(25, 25);
+            this.panelTopLeft.TabIndex = 1;
+            // 
+            // panelBottom
+            // 
+            this.panelBottom.Controls.Add(this.panelBottomMiddle);
+            this.panelBottom.Controls.Add(this.panelBottomRight);
+            this.panelBottom.Controls.Add(this.panelBottomLeft);
+            this.panelBottom.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panelBottom.Location = new System.Drawing.Point(0, 375);
+            this.panelBottom.Name = "panelBottom";
+            this.panelBottom.Size = new System.Drawing.Size(600, 25);
+            this.panelBottom.TabIndex = 1;
+            // 
+            // panelBottomMiddle
+            // 
+            this.panelBottomMiddle.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panelBottomMiddle.BackgroundImage")));
+            this.panelBottomMiddle.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelBottomMiddle.Location = new System.Drawing.Point(25, 0);
+            this.panelBottomMiddle.Name = "panelBottomMiddle";
+            this.panelBottomMiddle.Size = new System.Drawing.Size(550, 25);
+            this.panelBottomMiddle.TabIndex = 3;
+            // 
+            // panelBottomRight
+            // 
+            this.panelBottomRight.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panelBottomRight.BackgroundImage")));
+            this.panelBottomRight.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panelBottomRight.Location = new System.Drawing.Point(575, 0);
+            this.panelBottomRight.Name = "panelBottomRight";
+            this.panelBottomRight.Size = new System.Drawing.Size(25, 25);
+            this.panelBottomRight.TabIndex = 2;
+            // 
+            // panelBottomLeft
+            // 
+            this.panelBottomLeft.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panelBottomLeft.BackgroundImage")));
+            this.panelBottomLeft.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panelBottomLeft.Location = new System.Drawing.Point(0, 0);
+            this.panelBottomLeft.Name = "panelBottomLeft";
+            this.panelBottomLeft.Size = new System.Drawing.Size(25, 25);
+            this.panelBottomLeft.TabIndex = 1;
+            // 
+            // panelRight
+            // 
+            this.panelRight.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panelRight.BackgroundImage")));
+            this.panelRight.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panelRight.Location = new System.Drawing.Point(575, 25);
+            this.panelRight.Name = "panelRight";
+            this.panelRight.Size = new System.Drawing.Size(25, 350);
+            this.panelRight.TabIndex = 2;
+            // 
+            // panelLeft
+            // 
+            this.panelLeft.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panelLeft.BackgroundImage")));
+            this.panelLeft.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panelLeft.Location = new System.Drawing.Point(0, 25);
+            this.panelLeft.Name = "panelLeft";
+            this.panelLeft.Size = new System.Drawing.Size(25, 350);
+            this.panelLeft.TabIndex = 3;
+            // 
+            // panelMiddle
+            // 
+            this.panelMiddle.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panelMiddle.BackgroundImage")));
+            this.panelMiddle.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelMiddle.Location = new System.Drawing.Point(25, 25);
+            this.panelMiddle.Name = "panelMiddle";
+            this.panelMiddle.Size = new System.Drawing.Size(550, 350);
+            this.panelMiddle.TabIndex = 0;
+            // 
+            // PanelShield
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Transparent;
+            this.Controls.Add(this.panelMiddle);
+            this.Controls.Add(this.panelLeft);
+            this.Controls.Add(this.panelRight);
+            this.Controls.Add(this.panelBottom);
+            this.Controls.Add(this.panelTop);
+            this.Name = "PanelShield";
+            this.panelTop.ResumeLayout(false);
+            this.panelBottom.ResumeLayout(false);
+            this.ResumeLayout(false);
+        }
+        private void LoadUserControl()
+        {
+            if (_userControl != null)
+            {
+                this.SuspendLayout();
+                this.Width = _userControl.Width + 50;
+                this.Height = _userControl.Height + 50;
+                _userControl.Dock = DockStyle.Fill;
+                panelMiddle.Controls.Add(_userControl);
+                this.ResumeLayout(true);
+                this.Invalidate();
+            }
+        }
+        #endregion
+    }
+}
---
+++ Graphics/UserControlCustom.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows.Forms;
+
+namespace Tools4Libraries
+{
+    public class UserControlCustom : UserControl
+    {
+        public virtual void RefreshData()
+        {
+
+        }
+        public virtual void ChangeLanguage()
+        {
+
+        }
+    }
+}
