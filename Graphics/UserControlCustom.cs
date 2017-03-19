6c1c999aef6298ebf175c06fa42ca0adb7faf304
Thibault MONTAUFRAY <amostrack@gmail.com>
2017-03-19 21:57:29 +1300

    sunday happy dev !

--- Graphics/UserControlCustom.cs
+++ Graphics/UserControlCustom.cs
@@ -3,9 +3,9 @@
 namespace Tools4Libraries
 {
     public delegate void UserControlCustomEventHandler(object o);
-    public abstract class UserControlCustom : UserControl
+    public class UserControlCustom : UserControl
     {
-        public abstract event UserControlCustomEventHandler HeightChanged;
+        public virtual event UserControlCustomEventHandler HeightChanged;
         public virtual void RefreshData()
         {
 
